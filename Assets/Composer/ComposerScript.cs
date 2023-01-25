bb9706ff1a2fe488e7a9920d60d3d5bcd6047123
Horizox <pt.horizon@gmail.com>
2023-01-24 16:38:35 -0800

    no message

--- Assets/Composer/ComposerScript.cs
+++ Assets/Composer/ComposerScript.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Composer : MonoBehaviour
 {
+    public AudioSource a4, a4Sharp;
+
+    private List<AudioSource> notesInComposition = new List<AudioClip>();
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        notesInComposition.Add(a4);
+        notesInComposition.Add(a4);
+        notesInComposition.Add(a4Sharp);
+        notesInComposition.Add(a4);
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (TouchScreenKeyboard.current.aKey.wasPressedThis)
+        if(Keyboard.current.digit0Key.wasPressedThisframe)
+        {
+            notesInComposition[0].Play();
+        }
+
+        if (Keyboard.current.digit1Key.wasPressedThisframe)
+        {
+            notesInComposition[2].Play();
+        }
+
+        if (Keyboard.current.aKey.wasPressedThisFrame)
+
+            a4.Play();
     }
 }
