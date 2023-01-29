09384b65a5484c84e1dda6963c9d7e4c6c7914c6
Horizox <pt.horizon@gmail.com>
2023-01-28 17:22:47 -0800

    no message

--- Assets/Balls/BallScript.cs
+++ Assets/Balls/BallScript.cs
@@ -1,9 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class Ball_Script : MonoBehaviour
+public class BallScript : MonoBehaviour
 {
+
+
+    public GameObject sphere;
+    public GameObject button;
+    public TextMeshProUGUI OutputText;
+    public TMP_InputField Xinputfield;
+    public TMP_InputField Yinputfield;
+    public TMP_InputField Zinputfield;
+
+
+
+
+
+    public void GoThere()
+    {
+        sphere.name = "Parsa's Ball";
+
+        Transform t = sphere.GetComponent<Transform>();
+
+        int XinputSub = int.Parse(Xinputfield.text);
+        int YinputSub = int.Parse(Yinputfield.text);
+        int ZinputSub = int.Parse(Zinputfield.text);
+
+
+
+        if (XinputSub == 0 || YinputSub == 0 || ZinputSub == 0)
+        {
+
+            OutputText.text = "Please input a value in each respective Axis.";
+
+        }
+
+        else
+        {
+
+            t.position = new Vector3(XinputSub, YinputSub, ZinputSub);
+            OutputText.text = "There We Go!";
+
+        }
+        
+    }
     // Start is called before the first frame update
     void Start()
     {
--- Assets/GuessingGame/GuessingGame.cs
+++ Assets/GuessingGame/GuessingGame.cs
@@ -9,60 +9,66 @@ using UnityEngine;
 // MonoBehaviour - Indicates this file is a C# script.
 public class GuessingGame : MonoBehaviour
 {
-    // A reference to the Text object
-    public TextMeshProUGUI textGameObject;
-    public TMP_InputField inputUpperBound;
-    public int upperint = Int32.Parse(inputUpperBound.Text);
-    public TMP_InputField inputLowerBound;
-    public int lowerint - Int32.Parse(inputLowerBound.Text);
-    public TMP_InputField inputAnswer;
-    public int answerint = Int32.Parse(inputAnswer.Text);
-    public int guessNumber = rnd.Next(lowerint, upperint);
+    public TextMeshProUGUI outputText;
+    public TMP_InputField inputField;
 
-    // public - indicates Unity can refer to this function in the editor,
-    // such as to trigger when a button is pressed.
-    public void MyFunction()
+    public GameObject button;
+
+    private int selectedNumber = 0;
+
+    private void Start()
     {
-        textGameObject.text = "Hello!\nPlease input a lower bound and upper bound number below. Once finished please press the button to start the Game!";
-        if (inputAnswer.text != guessNumber)
+        outputText.text = "Please enter an upper bound integer for the guessing game!";
+    }
+
+    // This will be called when the Submit button is pressed.
+    public void OnSubmit()
+    {
+        // int: the TYPE
+        // myVariable: a name (anything, pretty much)
+        // = 5: assign its value.
+        // int myVariable = 5;
+
+        // Strong typing in C# prevents this!
+        // int myOtherVariable = "Erik";
+
+        // var myOtherVariable = Something();
+
+        int submittedNumber = int.Parse(inputField.text);
+
+        // If my selected number is equal to zero,
+        // i.e., if I have NOT yet selected a number.
+        if (selectedNumber == 0)
         {
-            if (answerint > guessNumber)
-            {
+            selectedNumber = Random.Range(1, submittedNumber + 1);
 
-                textGameObject.text = "The number you have chosen is too high. Please guess again!";
+            outputText.text = $"Guess a number between 1 and {submittedNumber}!";
 
-
-
-            } 
-            
-            else
-            {
-
-                textGameObject.text = "The number you have chosen is too low. Please guess again!";
-
-            }
-
-
+            Debug.Log(selectedNumber);
         }
+        // Otherwise, perform the guessing.
         else
         {
+            if (submittedNumber > selectedNumber)
+            {
+                // Too high!
+                outputText.text = "Too high!";
+            }
+            else if (submittedNumber < selectedNumber)
+            {
+                // Too low!
+                outputText.text = "Too low!";
+            }
+            else
+            {
+                outputText.text = "You got it!!";
 
-            textGameObject.text = "Congratulations! You have gussed the correct number! You Win!";
-
+                // Same as ticking the little tickmark next to the game object's name.
+                button.gameObject.SetActive(false);
+                inputField.gameObject.SetActive(false);
+            }
         }
-        // Function call to print to the console.
-        // Debug.Log(inputFieldGameObject.text);
-    }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        inputField.text = "";
     }
 }
