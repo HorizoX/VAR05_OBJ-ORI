c8b9d7a0a8abc15acc8aeb60b49724c422dde5e1
Horizox <pt.horizon@gmail.com>
2023-02-02 13:22:11 -0800

    Everything should BE OK NOW!

--- Assets/Race!/Scripts/RaceScript.cs
+++ Assets/Race!/Scripts/RaceScript.cs
@@ -1,74 +1,74 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class RaceScript : MonoBehaviour
-{
+//public class RaceScript : MonoBehaviour
+//{
 
-    public int courseLength;
-    public int regularTerrainChance = 25;
-    public int muddyTerrainChance = 12;
-    public int finishLine;
+//    public int courseLength;
+//    public int regularTerrainChance = 25;
+//    public int muddyTerrainChance = 12;
+//    public int finishLine;
 
-    private string playerRunner;
-    private string AIRunner = "A";
+//    private string playerRunner;
+//    private string AIRunner = "A";
 
-    private int playerRunnerPos = 0;
-    private int AIRunnerPos = 0;
+//    private int playerRunnerPos = 0;
+//    private int AIRunnerPos = 0;
 
-    private bool raceFinished = false;
+//    private bool raceFinished = false;
 
-    void Start()
-    {
-        // Prompt user to choose their runner character
-        Debug.Log("Choose your runner character: ");
-        //playerRunner = Console.ReadLine();
+//    void Start()
+//    {
+//        // Prompt user to choose their runner character
+//        Debug.Log("Choose your runner character: ");
+//        //playerRunner = Console.ReadLine();
 
-        StartCoroutine(RaceCoroutine());
-    }
+//        StartCoroutine(RaceCoroutine());
+//    }
 
-    IEnumerator RaceCoroutine()
-    {
-        while (!raceFinished)
-        {
-            yield return new WaitForSeconds(0.5f);
-            MoveRunner(ref playerRunnerPos, regularTerrainChance, muddyTerrainChance);
-            MoveRunner(ref AIRunnerPos, regularTerrainChance, muddyTerrainChance);
+//    IEnumerator RaceCoroutine()
+//    {
+//        while (!raceFinished)
+//        {
+//            yield return new WaitForSeconds(0.5f);
+//            MoveRunner(ref playerRunnerPos, regularTerrainChance, muddyTerrainChance);
+//            MoveRunner(ref AIRunnerPos, regularTerrainChance, muddyTerrainChance);
 
-            if (playerRunnerPos >= finishLine || AIRunnerPos >= finishLine)
-            {
-                raceFinished = true;
-                break;
-            }
-        }
+//            if (playerRunnerPos >= finishLine || AIRunnerPos >= finishLine)
+//            {
+//                raceFinished = true;
+//                break;
+//            }
+//        }
 
-        // Display results
-        if (playerRunnerPos >= finishLine)
-        {
-            Debug.Log(playerRunner + " wins!");
-        }
-        else
-        {
-            Debug.Log(AIRunner + " wins!");
-        }
-    }
+//        // Display results
+//        if (playerRunnerPos >= finishLine)
+//        {
+//            Debug.Log(playerRunner + " wins!");
+//        }
+//        else
+//        {
+//            Debug.Log(AIRunner + " wins!");
+//        }
+//    }
 
-    private void MoveRunner(ref int runnerPos, int regularTerrainChance, int muddyTerrainChance)
-    {
-        int randomNum = Random.Range(0, 100);
-        if (runnerPos % 5 == 0)
-        {
-            if (randomNum <= muddyTerrainChance)
-            {
-                runnerPos++;
-            }
-        }
-        else
-        {
-            if (randomNum <= regularTerrainChance)
-            {
-                runnerPos++;
-            }
-        }
-    }
-}
+//    private void MoveRunner(ref int runnerPos, int regularTerrainChance, int muddyTerrainChance)
+//    {
+//        int randomNum = Random.Range(0, 100);
+//        if (runnerPos % 5 == 0)
+//        {
+//            if (randomNum <= muddyTerrainChance)
+//            {
+//                runnerPos++;
+//            }
+//        }
+//        else
+//        {
+//            if (randomNum <= regularTerrainChance)
+//            {
+//                runnerPos++;
+//            }
+//        }
+//    }
+//}
